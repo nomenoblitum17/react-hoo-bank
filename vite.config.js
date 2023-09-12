91512da95d3a4c1677eb1ddffae0797335d204df
moises zurita <mezr.developer@gmail.com>
2023-09-12 10:59:07 -0430

    fourth upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://nomenoblitum17.github.io/react-hoo-bank"
+  base: "http://nomenoblitum17.github.io/react-hoo-bank"
 })
